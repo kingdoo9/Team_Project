08fbd9026e5e933425d75800e3446e18c6b15970
sinsubin <s2bj213@naver.com>
2015-11-30 15:56:44 +0900

    subin

--- src/MusicManager.cpp
+++ src/MusicManager.cpp
@@ -107,20 +107,19 @@ string MusicManager::loadMusic(const std::string &file){
 				// 이 음악이 이미 불러오지 않았는지 확인한다.
 				map<string,Music*>::iterator it=musicCollection.find(obj1->value[0]);
 				if(it==musicCollection.end()){
-					// 
-					//We've found an entry for a music file.
+					// 음악 파일의 입구를 찾는다.
 					Music* music=new Music;
 					music->music=NULL;
 					music->loop=NULL;
 
-					//Load some data.
+					// 데이터를 불러온다
 					for(map<string,vector<string> >::iterator i=obj1->attributes.begin();i!=obj1->attributes.end();++i){
 						if(i->first=="file"){
-							//Load the music file.
+							// 음악 파일을 불러온다.
 							music->music=Mix_LoadMUS((getDataPath()+"music/"+i->second[0]).c_str());
 						}
 						if(i->first=="loopfile"){
-							//Load the loop file.
+							// 순환 파일을 불러온다.
 							music->loop=Mix_LoadMUS((getDataPath()+"music/"+i->second[0]).c_str());
 						}
 						if(i->first=="trackname"){
@@ -146,15 +145,16 @@ string MusicManager::loadMusic(const std::string &file){
 						}
 					}
 
-					//Set the default value for lastTime.
+					// 마지막시간의 default 값을 설정한다.
 					music->lastTime=-1;
 					music->name=obj1->value[0];
 
-					//Now add it to the collection.
+					// 모음집에 추가한다.
 					musicCollection[obj1->value[0]]=music;
 				}
 
-				//Add the name of the music to the return string even if it's already loaded.
+				// 음악의 이름을 반환 string에 추가한다. 이미 그걸 불러왔더라도
+				// 이것은 음악 목록에 곱하는것을 허락한다..?
 				//This is to allow music to be in multiple music lists.
 				if(!returnString.empty())
 					returnString+=',';
@@ -163,18 +163,18 @@ string MusicManager::loadMusic(const std::string &file){
 		}
 	}
 
-	//Return the return string.
+	// 반환 string을 반환한다.
 	return returnString;
 }
 
 bool MusicManager::loadMusicList(const std::string &file){
-	//Open the .list file.
+	// .list 파일을 연다
 	ifstream musicFile;
 	musicFile.open(file.c_str());
 
-	//Check if the file exists.
+	// 만약 파일이 존재하면 체크한다.
 	if(musicFile){
-		//Now parse the file.
+		// 그 파일을 전달한다.
 		TreeStorageNode obj;
 		{
 			POASerializer objSerializer;
@@ -184,7 +184,7 @@ bool MusicManager::loadMusicList(const std::string &file){
 			}
 		}
 
-		//Get the name of the list.
+		// 목록들의 이름을 받는다.
 		std::string name;
 		{
 			map<string,vector<string> >::iterator it=obj.attributes.find("name");
@@ -196,18 +196,18 @@ bool MusicManager::loadMusicList(const std::string &file){
 			}
 		}
 
-		//Check if the list isn't already loaded.
+		// 만약 리스트들을 이미 불러오지 않았으면 체크한다.
 		std::map<std::string,std::vector<std::string> >::iterator it=musicLists.find(name);
 		if(it!=musicLists.end())
 			return true;
 
-		//Loop through the entries.
+		// entry들을 통해 순환한다.
 		for(unsigned int i=0;i<obj.subNodes.size();i++){
 			TreeStorageNode* obj1=obj.subNodes[i];
 			if(obj1==NULL)
 				continue;
 			if(obj1->value.size()>=1 && obj1->name=="musicfile"){
-				//Load the music file.
+				// 음악 파일을 불러온다.
 				string result=loadMusic(getDataPath()+"music/"+obj1->value[0]);
 				if(!result.empty()){
 					if(result.find(',')!=string::npos){
@@ -226,26 +226,27 @@ bool MusicManager::loadMusicList(const std::string &file){
 		return false;
 	}
 
-	//Nothing went wrong so return true.
+	// 아무것도 틀린게 없어서 참인 값을 반환한다.
 	return true;
 }
 
 void MusicManager::playMusic(const std::string &name,bool fade){
-	//Make sure music is enabled.
+	// 음악이 실행되는데 당연하다.
 	if(!enabled)
 		return;
 
-	//Check if the music is in the collection.
+	// 만약 음아이 모음집에 있으면 체크한다.
 	Music* music=musicCollection[name];
 	if(music==NULL){
 		cerr<<"ERROR: Unable to play music "<<name<<endl;
 		return;
 	}
 
+	// 만약 우리가 앞에 하나가 희미해지면 체크한다.
 	//Now check if we should fade the previous one out.
 	if(fade){
 	  	Mix_FadeOutMusic(375);
-		//Set the next music.
+		// 다음 음악을 세팅한다.
 		nextMusic=name;
 	}else{
 		if(music->loopStart<=0 && music->loop==NULL){
@@ -255,13 +256,13 @@ void MusicManager::playMusic(const std::string &name,bool fade){
 		}
 		Mix_VolumeMusic(music->volume*float(atoi(getSettings()->getValue("music").c_str())/float(MIX_MAX_VOLUME)));
 
-		//Set the playing pointer.
+		// playing 포인터를 설정한다.
 		playing=music;
 	}
 }
 
 void MusicManager::pickMusic(){
-	//Make sure the currentList exists.
+	// 현재목록이 존재하는게 당연하다
 	vector<std::string> list=musicLists[currentList];
 	if(currentList.empty()){
 		cerr<<"ERROR: Unkown music list "<<currentList<<endl;
