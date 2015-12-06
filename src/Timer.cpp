9d299dcaf74af74d00ae67fb1d9b2f58724f773a
unknown <>
2015-12-07 05:27:01 +0900

    chaelin

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,4 +1,4 @@
-/*
+/* 코드분석자 : 60142270 남채린
  * Copyright (C) 2011-2012 Me and My Shadow
  *
  * This file is part of Me and My Shadow.
@@ -22,10 +22,12 @@
 
 void Timer::start(){
 	//Store the current number of ticks (start time).
+	// tick(현재 시간)의 현재이 수 저장 
 	ticks=SDL_GetTicks();
 }
 
 int Timer::getTicks(){
 	//Return the differents with the current ticks and the start time.
+	// 현재의 tick과 시작시간을 가진 차이 반환 
 	return (SDL_GetTicks()-ticks);
 }
